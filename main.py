54d5a0ce2855e2eab42235e5e7f7ea7f23ca8eb5
100faker <studiobaek@hanmail.net>
2022-03-18 23:53:52 +0900

    -

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ async def on_ready():
   # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
   await client.change_presence(status=discord.Status.online)
 
-  await client.change_presence(activity=discord.Streaming(name="설윤이 직캠", url='https://www.youtube.com/watch?v=LTyc7mwLNys'))
+  await client.change_presence(activity=discord.Game(name="100만 원 준비"))
   #await client.change_presence(activity=discord.Game(name="게임 하는중"))
   #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
 
-  print("윤채원:",client.user.name,"954369596956217395:",client.user.id,"01:",discord.__version__)
+  print("김 대표:",client.user.name,"954390234555109427:",client.user.id,"01:",discord.__version__)
 
 
 client.run(os.environ['token'])
