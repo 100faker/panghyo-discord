60ef4664df6d09e3ceb762d9c8525ce09b695b37
100faker <studiobaek@hanmail.net>
2022-03-19 00:45:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,19 @@ async def on_message(message):
     return
 
   if message.content == "김대표 사기 아니야?":
-    await message.channel.send("사기 아니다 :pinching_hand: :pinching_hand: :pinching_hand:")
+    await message.channel.send("사기 아니다 :sa~1: :bar:")
 
   if message.content == "김대표":
-    await message.channel.send("안녕하세요. 팡머시티 회원 여러분.<br><br>업계 신용 보증수표.<br>저 김 대표가 팡머시티 회원 여러분들을 위해 화끈한 이벤트를 준비했습니다.<br><br>업계 최고 수준의 정확도와 신뢰도를 자랑하는 〈이상투자그룹〉과 협력하여 현재 무료 주식 정보 이벤트를 진행 중입니다.<br><br>최대 300% 수익률을 보장하는 확실한 정보를 모아 담은<br>〈이상투자그룹〉 서비스에 '무료 가입' 해주시면<br><br>저 김 대표가 여러분께 즉시 100만 원을 여러분에게 바로 쏴드리겠습니다.<br><br>저 믿고 〈이상투자그룹〉 가입하셔서 좋은 정보도 얻으시고<br>제가 드리는 100만 원도 꼭 받아가시기 바라겠습니다.:")
+    await message.channel.send("안녕하세요. 팡머시티 회원 여러분. 업계 신용 보증수표. 저 김 대표가 팡머시티 회원 여러분들을 위해 화끈한 이벤트를 준비했습니다. 업계 최고 수준의 정확도와 신뢰도를 자랑하는 〈이상투자그룹〉과 협력하여 현재 무료 주식 정보 이벤트를 진행 중입니다. 최대 300% 수익률을 보장하는 확실한 정보를 모아 담은 〈이상투자그룹〉 서비스에 '무료 가입' 해주시면 저 김 대표가 여러분께 즉시 100만 원을 여러분에게 바로 쏴드리겠습니다. 저 믿고 〈이상투자그룹〉 가입하셔서 좋은 정보도 얻으시고 제가 드리는 100만 원도 꼭 받아가시기 바라겠습니다.")
+
+  if message.content == "승트":
+    await message.channel.send(":heart~1:")
+
+  if message.content == "이상투자그룹":
+    await message.channel.send("참 좋은 회사입니다. 이상투자그룹 오늘 무료 가입하시면 제가 바로 100만 원 쏩니다")
+
+  if message.content == "인천대":
+    await message.channel.send("죄송합니다")
 
   print("김 대표:",client.user.name,"954390234555109427:",client.user.id,"01:",discord.__version__)
 
